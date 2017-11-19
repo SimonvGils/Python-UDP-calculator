d8cb2718ed9b281d6240fe99f255f50e99d58db1
SimonvGils <32328906+SimonvGils@users.noreply.github.com>
2017-11-19 14:27:51 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,47 +1,30 @@
 from socket import *
-
 serverPort = 12456
-
 serverSocket = socket(AF_INET,SOCK_DGRAM)
-
 serverSocket.bind(('',12456))
-
 print ('The server is connected')
 
 
 while 1:
-	
 number1,  clientAddress  = serverSocket.recvfrom(2048)
-	
 calc, 	  clientAddress  = serverSocket.recvfrom(2048)	
-	
 number2,  clientAddress	 = serverSocket.recvfrom(2048)
 
 
 
 if calc == '+':
-	modifiedAddition = int(number1.decode()) + int(number2.decode())
-	
-modifiedAddition = str(modifiedAddition)
-	
-serverSocket.sendto(modifiedAddition.encode(), clientAddress)
-	
-print('You added up numbers')
-
+	modifiedAddition = int(number1.decode()) + int(number2.decode())	
+	modifiedAddition = str(modifiedAddition)	
+	serverSocket.sendto(modifiedAddition.encode(), clientAddress)
+	print('You added up numbers')
 else: 
 	pass
 
-
-
 if calc == '-':
 	modifiedAddition = int(number1.decode()) - int(number2.decode())
-	
-modifiedAddition = str(modifiedAddition)
-	
-serverSocket.sendto(modifiedAddition.encode(), clientAddress)
-	
-print ('You have succesfully subtracted')
-
+	modifiedAddition = str(modifiedAddition)
+	serverSocket.sendto(modifiedAddition.encode(), clientAddress)
+	print ('You have succesfully subtracted')
 else:
 	pass
  
