6a1279ba5a3bd0f2d7b639056389306c8692744d
Simon <gils.van.sja@gmail.com>
2017-11-20 01:15:20 +0000

    Version 0.2

--- client.py
+++ client.py
@@ -1,19 +1,34 @@
+
 from socket import * 
+import socket
+
 serverName = 'Localhost'
 serverport = 12456
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print 'Traget IP: ', serverName
+print 'Target Port:', serverport
+print '\n'
+
+MESSAGE = raw_input ('Input a message: ')
+number1 = raw_input ('Input number1: ')
+number2 = raw_input ('Input number2: ')
+operator = raw_input ('Select an operator: (+ / * -) ')
 
 
+clientSocket.sendto(MESSAGE,(serverName,serverport))
+clientSocket.sendto(number1.encode(),(serverName,serverport))
+clientSocket.sendto(number2.encode(),(serverName,serverport))
+clientSocket.sendto(operator.encode(),(serverName,serverport))
 
-number1 = raw_input('Input number1: ')
-calc = raw_input('Calculation: +, - ,/ ,*: ')
-number2 = raw_input('Input number2: ')
+MESSAGE,serverAddress = clientSocket.recvfrom(2048)
+print MESSAGE
+number3,serverAddress = clientSocket.recvfrom(2048)
+print 'Sent back number 3: ', number3
+number4,serverAddress = clientSocket.recvfrom(2048)
+print 'Number4  send back: ', number4
+answer,serverAddress = clientSocket.recvfrom(2048)
+print 'your result: ', answer
 
-clientSocket.sendto(number1.encode(),('Localhost',12456))
-clientSocket.sendto(calc,('Localhost',12456))
-clientSocket.sendto(number2.encode(),('Localhost',12456))
-
-modifiedAddition, serverAddress=clientSocket.recvfrom(2048)
-print(modifiedAddition.decode())
 clientSocket.close()
 
--- server.py
+++ server.py
@@ -1,32 +1,50 @@
 from socket import *
+import socket
+
+serverName = 'Localhost'
 serverPort = 12456
-serverSocket = socket(AF_INET,SOCK_DGRAM)
-serverSocket.bind(('',12456))
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverSocket.bind(('localhost',12456))
 print ('The server is connected')
-
-
 while 1:
-number1,  clientAddress  = serverSocket.recvfrom(2048)
-calc, 	  clientAddress  = serverSocket.recvfrom(2048)	
-number2,  clientAddress	 = serverSocket.recvfrom(2048)
+	
+	MESSAGE, clientAddress = serverSocket.recvfrom(2048)
+	serverSocket.sendto(MESSAGE,clientAddress)
+	print 'Data has been received:', MESSAGE
+	print '\n'
 
+	number1, clientAddress = serverSocket.recvfrom(2048)
+	print 'Number1 received:', number1
 
+	number2, clientAddress = serverSocket.recvfrom(2048)
+	print 'Number2 received:', number2
+	
+	number3 = int(number1.decode())*2
+	number3 = str(number3)
+	serverSocket.sendto(number3.encode(),clientAddress)
+	number4 = int(number2.decode())*3
+	number4 = str(number4)
+	serverSocket.sendto(number4.encode(),clientAddress)
+	
+	operator, clientAddress = serverSocket.recvfrom(2048)
+	print 'received operator', operator
+	print 'calculating..'
 
-if calc == '+':
-	modifiedAddition = int(number1.decode()) + int(number2.decode())
-	modifiedAddition = str(modifiedAddition)
-	serverSocket.sendto(modifiedAddition.encode(), clientAddress)
-	print('You added up numbers')
-else: 
-	pass
+if str(operator) == '+':
+	answer = int(number1.decode()) + int(number2.decode())
+	answer = str(answer)
+	serverSocket.sendto(answer.encode(),clientAddress)
+elif str(operator) == '-':
+	answer = int(number1.decode()) - int(number2.decode())
+	answer = str(answer)
+	serverSocket.sendto(answer.encode(),clientAddress)
+elif str(operator) == '/':
+	answer = int(number1.decode()) / int(number2.decode())
+	answer = str(answer)
+	serverSocket.sendto(answer.encode(),clientAddress)
+elif str(operator) == '*':
+	answer = int(number1.decode()) * int(number2.decode())
+	answer = str(answer)
+	sererSocket.sendto(answer.encode(),clientAddress)
 
-
-
-if calc == '-':
-	modifiedAddition = int(number1.decode()) - int(number2.decode())
-	modifiedAddition = str(modifiedAddition)
-	serverSocket.sendto(modifiedAddition.encode(), clientAddress)
-	print ('You have succesfully subtracted')
-else:
-	pass
- 
+serverSocket.close()
