0a43fd2ce150d8eb75a3525a60a32899e479c424
Simon <gils.van.sja@gmail.com>
2017-11-20 01:15:55 +0000

    Update client.py

--- client.py
+++ client.py
@@ -10,19 +10,18 @@ print 'Traget IP: ', serverName
 print 'Target Port:', serverport
 print '\n'
 
-MESSAGE = raw_input ('Input a message: ')
+
 number1 = raw_input ('Input number1: ')
 number2 = raw_input ('Input number2: ')
 operator = raw_input ('Select an operator: (+ / * -) ')
 
 
-clientSocket.sendto(MESSAGE,(serverName,serverport))
+
 clientSocket.sendto(number1.encode(),(serverName,serverport))
 clientSocket.sendto(number2.encode(),(serverName,serverport))
 clientSocket.sendto(operator.encode(),(serverName,serverport))
 
-MESSAGE,serverAddress = clientSocket.recvfrom(2048)
-print MESSAGE
+
 number3,serverAddress = clientSocket.recvfrom(2048)
 print 'Sent back number 3: ', number3
 number4,serverAddress = clientSocket.recvfrom(2048)
